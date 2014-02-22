ab55c50842e674d042a42d0a614ee04fd75d0c4b
jorgetr <jorgetr@galileo.edu>
2014-02-21 23:38:30 -0600

    demo3

--- demo3/gen/com/sp2014/demo3/R.java
+++ demo3/gen/com/sp2014/demo3/R.java
@@ -34,13 +34,14 @@ public final class R {
         public static final int venezuela=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int btnPhoto=0x7f080002;
+        public static final int action_settings=0x7f080007;
+        public static final int btnPhoto=0x7f080003;
         public static final int btnfromGallery=0x7f080000;
-        public static final int grid=0x7f080003;
-        public static final int img=0x7f080001;
-        public static final int imgflag=0x7f080004;
-        public static final int txtName=0x7f080005;
+        public static final int btnfromcamera=0x7f080001;
+        public static final int grid=0x7f080004;
+        public static final int img=0x7f080002;
+        public static final int imgflag=0x7f080005;
+        public static final int txtName=0x7f080006;
     }
     public static final class layout {
         public static final int activity_camera=0x7f030000;
@@ -54,12 +55,13 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int btn_from_camera=0x7f050005;
         public static final int btn_from_gallery=0x7f050004;
         public static final int btnphoto=0x7f050003;
         public static final int hello_world=0x7f050002;
-        public static final int msg_no=0x7f050006;
-        public static final int msg_yes=0x7f050005;
-        public static final int title_activity_camera=0x7f050007;
+        public static final int msg_no=0x7f050007;
+        public static final int msg_yes=0x7f050006;
+        public static final int title_activity_camera=0x7f050008;
     }
     public static final class style {
         /** 
--- demo3/src/com/sp2014/demo3/activities/CameraActivity.java
+++ demo3/src/com/sp2014/demo3/activities/CameraActivity.java
@@ -1,11 +1,19 @@
 package com.sp2014.demo3.activities;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
@@ -17,11 +25,17 @@ import com.sp2014.demo3.R;
 
 public class CameraActivity extends Activity implements OnClickListener {
 	private static final int LOAD_IMAGE =1;
+	private static final int CAMARE =2;
+	Button btn;
+	Button btnCamera;
+	String photoPath;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_camera);
-		Button btn = (Button)findViewById(R.id.btnfromGallery);
+		btn = (Button)findViewById(R.id.btnfromGallery);
+		btnCamera = (Button)findViewById(R.id.btnfromcamera);
+		btnCamera.setOnClickListener(this);
 		btn.setOnClickListener(this);
 	}
 
@@ -31,35 +45,108 @@ public class CameraActivity extends Activity implements OnClickListener {
 		getMenuInflater().inflate(R.menu.camera, menu);
 		return true;
 	}
+	
+	  public Bitmap resizeBitmap(int targetW, int targetH) {
+			BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+			bmOptions.inJustDecodeBounds = true;
+			BitmapFactory.decodeFile(photoPath, bmOptions);
+			int photoW = bmOptions.outWidth;
+			int photoH = bmOptions.outHeight;
+
+			int scaleFactor = 1;
+			if ((targetW > 0) || (targetH > 0)) {
+				scaleFactor = Math.min(photoW/targetW, photoH/targetH);	
+			}
+
+			bmOptions.inJustDecodeBounds = false;
+			bmOptions.inSampleSize = scaleFactor;
+			bmOptions.inPurgeable = true;
+
+			return BitmapFactory.decodeFile(photoPath, bmOptions);    	
+	    }
 
 	@Override
 	public void onClick(View v) {
-		Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-		startActivityForResult(i, LOAD_IMAGE);
+		Intent intent =null;
+		int code =0;
+		if(v.getId()==btn.getId()){
+			intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+			code=LOAD_IMAGE;
+		}else if(v.getId()==btnCamera.getId()){
+			intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			File photo= setupFile();
+			 photoPath = photo.getAbsolutePath();
+			intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+			code=CAMARE;
+		}
+		
+		startActivityForResult(intent, code);
+	}
+	
+
+	private File setupFile() {
+		File albumDir;
+		String albumName="ejemplo";
+		if (Build.VERSION.SDK_INT >=  Build.VERSION_CODES.FROYO){
+				albumDir= new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),albumName);
+			}else {
+				albumDir=new File(Environment.getExternalStorageDirectory() + "/dcmi"+albumName);
+			}
+		albumDir.mkdirs();
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",Locale.getDefault()).format(Calendar.getInstance().getTime());
+		String imageFileName = "IMG_"+ timeStamp +".jpg";
+		File image = new File(albumDir+"/"+imageFileName);
+		return image;
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 				super.onActivityResult(requestCode, resultCode, data);
 				
-			if(requestCode == LOAD_IMAGE && resultCode == RESULT_OK && data != null){
-				Uri selectedImage = data.getData();
-				String[] filePathColumn ={MediaStore.Images.Media.DATA};
-				Cursor cursor = getContentResolver().query(selectedImage, filePathColumn,null,null,null);
-				
-				if (cursor.moveToFirst()){
-					int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-					String picturePath = cursor.getString(columnIndex);
-					cursor.close();
-					
-					ImageView img =(ImageView)findViewById(R.id.img);
-					img.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-					
+				switch (requestCode){
+				case LOAD_IMAGE:
+					if (resultCode == RESULT_OK){
+						fromGallery(data);
+					}
+					break;
+				case CAMARE:
+					if (resultCode == RESULT_OK){
+						fromCamera(data);
+					}
+					break;
 				}
+	}
+	public void fromCamera (Intent data){
+		
+		ImageView img =(ImageView)findViewById(R.id.img);
+		Bitmap bitmap = resizeBitmap(img.getWidth(), img.getHeight());
+		img.setImageBitmap(bitmap);
+		Intent mediaScan =new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
+		File file =new File(photoPath);
+		Uri contentUri = Uri.fromFile(file);
+		mediaScan.setData(contentUri);
+		sendBroadcast(mediaScan);
+	}
+	
+	public void fromGallery (Intent data){
+		if(data != null){
+			Uri selectedImage = data.getData();
+			String[] filePathColumn ={MediaStore.Images.Media.DATA};
+			Cursor cursor = getContentResolver().query(selectedImage, filePathColumn,null,null,null);
+			
+			if (cursor.moveToFirst()){
+				int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+				String picturePath = cursor.getString(columnIndex);
+				cursor.close();
 				
+				ImageView img =(ImageView)findViewById(R.id.img);
+				img.setImageBitmap(BitmapFactory.decodeFile(picturePath));
 				
 			}
-	}
+			
+			
+		}
+	}	
 
 	
 }
